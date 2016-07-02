1056be855950774c6ba07309c5f65b36725341c3
dark419 <mr.ahlee@gmail.com>
2016-07-03 08:54:05 +1000

    minor change

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+/* A counter button: tap the button to increase the count.*/
+
 class Counter extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0;
+      count: 0,
     };
   }
 
